3a78cabcc206cfc4eee6252b21299b7a19e9dd1a
E-Gaming551 <victorezehi@icloud.com>
2025-03-01 14:00:06 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function checkForm() {
+	var username = document.forms["myForm"]["username"].value;
+	var password = document.forms["myForm"]["password"].value;
+	var firstname = document.forms["myForm"]["firstname"].value;
+	var lastname = document.forms["myForm"]["lastname"].value;
+	var username = document.forms["myForm"]["username"].value;
+	var email = document.forms["myForm"]["email"].value;
+}
